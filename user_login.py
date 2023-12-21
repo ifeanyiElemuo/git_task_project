2b1cc8643534c752ca981a34e596be00bfe0c2e5
Darfy <darfy009@gmail.com>
2023-12-21 12:06:01 +0000

    Initial commit

---
+++ user_login.py
@@ -0,0 +1,8 @@
+admin_username = 'admin'
+admin_password = 'password'
+
+username = input("Username: ")
+password = input("Password: ")
+
+if username == admin_username and password == admin_password:
+    print('\nLogin successful!')
