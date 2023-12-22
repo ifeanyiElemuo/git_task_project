afc60dd541acd487c862951ff982a312dd6923bc
Darfy <darfy009@gmail.com>
2023-12-22 13:25:14 +0000

    Added input validation and login failed message

--- user_login.py
+++ user_login.py
@@ -1,8 +1,10 @@
 admin_username = 'admin'
 admin_password = 'password'
 
-username = input("Username: ")
+username = input("Username: ").lower()
 password = input("Password: ")
 
 if username == admin_username and password == admin_password:
     print('\nLogin successful!')
+else:
+    print('\nLogin failed! Incorrect username or password.')
