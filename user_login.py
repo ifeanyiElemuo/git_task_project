14ff692a51fe0bef21e3765576e1cd148a0bd004
Darfy <darfy009@gmail.com>
2023-12-22 13:02:03 +0000

    Added comments to describe code functionality

--- user_login.py
+++ user_login.py
@@ -1,8 +1,11 @@
+# create admin account with admin username and password
 admin_username = 'admin'
 admin_password = 'password'
 
+# for user log in, prompt user for username and password
 username = input("Username: ")
 password = input("Password: ")
 
+# check if user input matches the login parameters for admin
 if username == admin_username and password == admin_password:
     print('\nLogin successful!')
